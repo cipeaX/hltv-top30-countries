abf48287848ef34c9049257febac3229f2cf98c3
Aron Joris Striebich <a.striebich@tu-berlin.de>
2023-10-12 22:17:16 +0200

    add empty.html

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function updateCountry() {
     i = document.getElementById("plot")
     country = s.value
     if(country === "Select Country"){
-        i.src = ""
+        i.src = "./data/empty.html"
     } else {
         i.src = "./data/plots/" + country + ".html"
         document.getElementById("plot").src = document.getElementById("plot").src
